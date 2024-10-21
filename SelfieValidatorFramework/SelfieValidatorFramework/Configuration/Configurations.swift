fa1b2586781bb4f01049e43b0fcfad8810f0d419
Ahmed Attalla <devahmedattalla@gmail.com>
2024-10-21 03:38:42 +0300

    Added docs to configurations

--- SelfieValidatorFramework/SelfieValidatorFramework/Configuration/Configurations.swift
+++ SelfieValidatorFramework/SelfieValidatorFramework/Configuration/Configurations.swift
@@ -1,15 +1,25 @@
 import UIKit
 
+/// An enumeration that holds configuration settings for the SelfieValidator framework.
 public enum Configurations {
+    /// A delegate that conforms to the `SelfieValidatorDelegate` protocol.
+    /// This delegate is used to handle events and actions related to the SelfieValidator.
+    /// The delegate can be set privately within the framework but can be accessed publicly.
     public private(set) static var delegate: SelfieValidatorDelegate?
 }
 
+/// Sets up the `Configurations` with the provided delegate.
+///
+/// - Parameter delegate: An instance conforming to `SelfieValidatorDelegate` that will handle validation events.
 public extension Configurations {
     static func setup(delegate: SelfieValidatorDelegate) {
         Configurations.delegate = delegate
     }
 }
 
+/// Returns an instance of `CameraViewController` to be used for selfie validation.
+///
+/// - Returns: A `UIViewController` instance representing the selfie validation scene.
 public func getSelfieValidationScene() -> UIViewController {
     CameraViewController()
 }
--- SelfieValidatorFramework/SelfieValidatorFramework/Configuration/SelfieValidatorDelegate.swift
+++ SelfieValidatorFramework/SelfieValidatorFramework/Configuration/SelfieValidatorDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 
 public protocol SelfieValidatorDelegate: AnyObject {
+    /// Delegate method that is called when a selfie is captured.
+    /// - Parameter image: The captured selfie image.
     func didCaptureSelfie(_ image: UIImage)
 }
